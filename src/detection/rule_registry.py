f0c424089be49c5e24ef2821b7a274b7eba8661d
Kailas <djbpm178@gmail.com>
2026-02-17 19:44:14 +0530

    refactor: standardize rule registry with class-based rules

--- src/detection/rule_registry.py
+++ src/detection/rule_registry.py
@@ -2,6 +2,7 @@ from src.detection.rules.ssh_bruteforce import SSHBruteForceRule
 from src.detection.rules.suspicious_login_after_bruteforce import SuspiciousLoginAfterBruteForceRule
 from src.detection.rules.privilege_escalation_sudo import SudoPrivilegeEscalationRule
 
+
 def get_all_rules():
     return [
         SSHBruteForceRule(),
